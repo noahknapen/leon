2061a8a7c906a3645f3baa8b90fe13386fde571f
Noah Knapen <noah.knapen@gmail.com>
2023-11-27 12:43:52 +0100

    feat: update encryption to use AES instead of basic encryption

--- app/src/js/localstorageapi.js
+++ app/src/js/localstorageapi.js
@@ -1,11 +1,40 @@
 import ls from 'localstorage-slim'
+import encUTF8 from 'crypto-js/enc-utf8'
+import AES from 'crypto-js/aes'
 
 export default class LocalStorageAPI {
   static setItem(key, data) {
-    ls.set(key, data, { encrypt: true })
+    ls.config.encrypt = true
+    ls.config.secret = 'leon'
+    ls.config.encrypter = (data, secret) =>
+      AES.encrypt(JSON.stringify(data), secret).toString()
+    ls.config.decrypter = (data, secret) => {
+      try {
+        return JSON.parse(AES.decrypt(data, secret).toString(encUTF8))
+      } catch (e) {
+        // incorrect/missing secret, return the encrypted data instead
+        return data
+      }
+    }
+
+    ls.set(key, data)
+    console.log(ls.get(key))
   }
 
   static getItem(key) {
-    return ls.get(key, { decrypt: true })
+    ls.config.encrypt = true
+    ls.config.secret = 'leon'
+    ls.config.encrypter = (data, secret) =>
+      AES.encrypt(JSON.stringify(data), secret).toString()
+    ls.config.decrypter = (data, secret) => {
+      try {
+        return JSON.parse(AES.decrypt(data, secret).toString(encUTF8))
+      } catch (e) {
+        // incorrect/missing secret, return the encrypted data instead
+        return data
+      }
+    }
+
+    return ls.get(key)
   }
 }
