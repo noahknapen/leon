a0e166332d151571ac9d7e37ead5543bd0b837ee
Noah Knapen <noah.knapen@gmail.com>
2023-12-10 19:55:38 +0100

    Added basic test for unencrypted localStorage

--- test/unit/app/chatbot.spec.js
+++ test/unit/app/chatbot.spec.js
@@ -1,19 +1,19 @@
 import Chatbot from '../../../app/src/js/chatbot'
-import lsapi from '../../../app/src/js/localstorageapi'
-jest.mock('../../../app/src/js/chatbot')
 
 
 describe('saveBubble()', () => {
   test('Check if bubble is saved in local storage, unencrypted', () => {
-    lsapi.setItem = jest.fn()
+    const mockSetItem = jest.spyOn(Storage.prototype, 'setItem')
     const chatbot = new Chatbot()
-    chatbot.parsedBubbles = {}
-    const bubble = { who: 'leon', text: 'Hello world' }
 
+    // Put the chatbot in the right state to test
+    chatbot.parsedBubbles = []
+    chatbot.noBubbleMessage = { classList: { contains: jest.fn(), add: jest.fn() } }
+    chatbot.feed = { scrollTo: jest.fn() }
+
+    // Call saveBubble() and check if the bubble is saved in local storage (where looking at source code reveals that the local storage is equal to parsedBubbles)
     chatbot.saveBubble('leon', 'Hello world')
-
-    //expect(chatbot.parsedBubbles).toBe([bubble])
-    //expect(chatbot.parsedBubbles).toIncludeAnyMembers([bubble])
-    expect(chatbot.saveBubble).toHaveBeenCalledTimes(1)
+    expect(chatbot.parsedBubbles).toStrictEqual([{ who: 'leon', string: 'Hello world' }])
+    expect(mockSetItem).toHaveBeenCalledTimes(1)
   })
 })
