53a97c2271a20d0cb74979d2e779a8cb8021824d
Noah Knapen <noah.knapen@gmail.com>
2023-12-11 11:39:37 +0100

    test: updated test to more accurately depict the unencrypted localStorage problem

--- test/unit/app/chatbot.spec.js
+++ test/unit/app/chatbot.spec.js
@@ -1,9 +1,31 @@
 import Chatbot from '../../../app/src/js/chatbot'
 
+class LocalStorageMock {
+  constructor() {
+    this.store = {}
+  }
+
+  clear() {
+    this.store = {}
+  }
+
+  getItem(key) {
+    return this.store[key] || null
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value)
+  }
+
+  removeItem(key) {
+    delete this.store[key]
+  }
+}
+
+global.localStorage = new LocalStorageMock()
 
 describe('saveBubble()', () => {
   test('Check if bubble is saved in local storage, unencrypted', () => {
-    const mockSetItem = jest.spyOn(Storage.prototype, 'setItem')
     const chatbot = new Chatbot()
 
     // Put the chatbot in the right state to test
@@ -14,6 +36,8 @@ describe('saveBubble()', () => {
     // Call saveBubble() and check if the bubble is saved in local storage (where looking at source code reveals that the local storage is equal to parsedBubbles)
     chatbot.saveBubble('leon', 'Hello world')
     expect(chatbot.parsedBubbles).toStrictEqual([{ who: 'leon', string: 'Hello world' }])
-    expect(mockSetItem).toHaveBeenCalledTimes(1)
+    expect(localStorage.getItem('bubbles')).not.toStrictEqual([
+      { who: 'leon', string: 'Hello world' }
+    ])
   })
 })
